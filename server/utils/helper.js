065a1d57e27ee2556a29995ccc1ff38e4daf4f8c
ajay sonere <ajaysonere786@gmail.com>
2025-10-07 23:23:45 +0530

    update news

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,5 +1,6 @@
 import { supportedMimes } from "../config/fileSystem.js";
 import { v4 as uuidv4 } from "uuid";
+import fs from 'fs';
 
 export const imageValidator = (size, mime) => {
 
@@ -22,4 +23,25 @@ export const randomNumGenenrator = () => {
 
 export const imageUrlTransformer = (imageName) => {
   return `${process.env.APP_URL}/images/${imageName}`
+}
+
+export const removeImage = (imageName) => {
+  const imagePath = process.cwd() + "/public/images/" + imageName;
+  if(fs.existsSync(imagePath)){
+    return fs.unlinkSync(imagePath);
+  }
+}
+
+export const uploadImage = (imageName) => {
+   const imageExtension = imageName?.name.split(".");
+   const newImageName = randomNumGenenrator() + "." + imageExtension[1];
+   
+   const uploadPath = process.cwd() + "/public/images/" + newImageName;
+   
+   imageName.mv(uploadPath , (err) => {
+      if(err){
+         return res.status(500).json({success:false, error:[{message:'failed to upload the image.'}]})
+      }
+   })
+   return newImageName;
 }
